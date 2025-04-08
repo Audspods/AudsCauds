e2035540c1489856dab43d2a4159f31cc076e697
Audspods <audreyshen7@gmail.com>
2025-04-07 23:02:40 -0700

    added new cauds

--- reactproject/src/pages/Home.jsx
+++ reactproject/src/pages/Home.jsx
@@ -2,11 +2,20 @@ import "../css/Home.css";
 import hereforyouImg from "../assets/hereforyou.jpg";
 import meowntainImg from "../assets/meowntain.jpg";
 import wermImg from "../assets/werm.jpg";
+import hookedImg from "../assets/hooked.jpg";
+import lawlessImg from "../assets/lawless.jpg";
+import storkedImg from "../assets/storked.jpg";
 
 export default function Home() {
   return (
     <div className="profile-container">
-      <div className="new">COMING SOON!</div>
+      <div className="new">COMING SOON!
+        <div className="new-cauds">
+          <img src={hookedImg} alt="hooked" className="caud-img" />
+          <img src={lawlessImg} alt="lawless" className="caud-img" />
+          <img src={storkedImg} alt="storked" className="caud-img" />
+        </div>
+      </div>
       <div className="title">Cauds</div>
       <div className="cauds">
         <img src={hereforyouImg} alt="hereforyou" className="caud-img" />
