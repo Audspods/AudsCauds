d8f5b4eac24cbe15911f5336aa9f8789d42d6672
Audspods <audreyshen7@gmail.com>
2025-04-13 12:23:15 -0700

    debug contacts more

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './css/App.css';
 import Home from './pages/Home';
 import About from "./pages/About";
