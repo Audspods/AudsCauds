1ab6d9c6c0037447c4326e9be62aaec90454e0c7
Audspods <audreyshen7@gmail.com>
2025-04-13 12:24:58 -0700

    contacts

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -10,18 +10,20 @@ import Footer from "./components/Footer";
 function App() {
   
   return (
-    <div className="page">
-      <Navbar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="page">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
